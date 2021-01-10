dfaf448e33c94b94b71aa43efd4a5ab6c47db939
duhow <contacto@duhowpi.net>
2021-01-10 16:22:06 +0100

    Add extended logging from rxi/log.c

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 LIBS = -lasound -lquiet -ljansson -lliquid -lfec
-CFLAGS ?= -O3
+CFLAGS ?= -O3 -DLOG_USE_COLOR=1
 CC = gcc
 
 
-hey-wifi: main.c
-	$(CC) $(CFLAGS) -o $@ $< $(LIBS)
+hey-wifi: main.c log.c
+	$(CC) $(CFLAGS) -o $@ $^ $(LIBS)
 
 clean:
 	@rm -vf hey-wifi
--- main.c
+++ main.c
@@ -2,12 +2,13 @@
 #include <string.h>
 #include <unistd.h>
 
-
+#include "log.h"
 #include <alsa/asoundlib.h>
 #include <quiet.h>
 
 #define PCM_DEVICE "default"
 #define PROFILE_FILE "quiet-profiles.json"
+#define SAMPLE_RATE 44100
 
 bool verbose = false;
 int err = 0;
@@ -23,45 +24,54 @@ int prepare_alsa() {
     snd_pcm_info_t *info;
     snd_pcm_info_alloca(&info);
 
+    log_debug("opening PCM handle %s", pcm_name);
     err = snd_pcm_open(&handle, pcm_name, SND_PCM_STREAM_CAPTURE, SND_PCM_NONBLOCK);
     if(err < 0){
-        printf("audio open error: %s", snd_strerror(err));
+        log_error("audio open error: %s", snd_strerror(err));
         return 1;
     }
 
+    log_debug("setting default info");
     err = snd_pcm_info(handle, info);
     if(err < 0){
-        printf("info error: %s", snd_strerror(err));
+        log_error("info error: %s", snd_strerror(err));
         return 1;
     }
 
+    log_debug("setting PCM as nonblock");
     err = snd_pcm_nonblock(handle, 1);
     if(err < 0){
-        printf("nonblock setting error: %s", snd_strerror(err));
+        log_error("nonblock setting error: %s", snd_strerror(err));
         return 1;
     }
 
     chunk_size = 1024;
-    audiobuf = (u_char *)malloc(1024);
 
+    log_debug("allocating memory");
+    audiobuf = (u_char *)malloc(1024);
     if(audiobuf == NULL){
-        printf("not enough memory");
+        log_error("not enough memory");
         return 1;
     }
 
+    log_debug("all done");
     return 0;
 }
 
 int prepare_quiet() {
+    log_debug("loading options from file: %s", config_file);
     quiet_decoder_options *decodeOpt =
         quiet_decoder_profile_filename(config_file, "audible");
 
-
+    log_debug("creating a decoder");
+    quiet_decoder *decoder = quiet_decoder_create(decodeOpt, SAMPLE_RATE);
 }
 
 int run() {
     err = prepare_alsa();
     if(err > 0){ return err; }
+
+    err = prepare_quiet();
 
     if(handle){
         snd_pcm_drain(handle);
@@ -73,33 +83,35 @@ int run() {
 
 int main(int argc, char *argv[]) {
     unsigned char i;
+    log_set_level(LOG_INFO);
 
     if(argc > 1){
         for(i = 1; i < argc; i++){
             if(strcmp("-v", argv[i]) == 0){
                 verbose = true;
+                log_set_level(LOG_TRACE);
                 continue;
             } else if(strcmp("-D", argv[i]) == 0){
                 if(argc <= i+1){
-                    printf("[!] Specify the PCM record device.\n");
+                    log_error("Specify the PCM record device.");
                     exit(1);
                 }
                 strcpy(pcm_name, argv[++i]);
                 continue;
             } else if(strcmp("-c", argv[i]) == 0){
                 if(argc <= i+1){
-                    printf("[!] Specify the config file to use.\n");
+                    log_error("Specify the config file to use.");
                     exit(1);
                 }
                 strcpy(config_file, argv[++i]);
                 continue;
             } else if(strcmp("-r", argv[i]) == 0){
                 if(argc <= i+1){
-                    printf("[!] Specify the config file to use.\n");
+                    log_error("Specify the executable script or program to use.");
                     exit(1);
                 }
             } else {
-                printf("[!] Parameter %s not recognized.\n", argv[i]);
+                log_error("Parameter %s not recognized.", argv[i]);
                 exit(1);
             }
         }
@@ -117,6 +129,7 @@ int main(int argc, char *argv[]) {
     printf("device: %s\n", pcm_name);
     printf("config: %s\n", config_file);
 
+    log_debug("starting program");
     return run();
 }
 
